e431b4df6b5a7e70c0f156da86edd578d7d49f8d
Abdullah Al MahMud <abdudevs@gmail.com>
2023-05-15 17:36:53 +0600

    added api call to the backend

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function FileUpload() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+
+      // Make a request to your backend API endpoint
+      const response = await axios.post(
+        "http://localhost:3000/api/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      // Access the public URL from the response
+      const publicURL = response.data.publicURL;
+
+      // Use the publicURL as needed (e.g., store it in MongoDB)
+      // ...
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
+}
+
+export default FileUpload;
